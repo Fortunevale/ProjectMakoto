f9e40cf17b4468abd291dbe3f424e0739e461413
XorogVEVO <xorog@outlook.de>
2022-01-26 00:33:20 +0100

    Added Update Pushed ExitCode

--- Project-Ichigo/ExitCodes.cs
+++ Project-Ichigo/ExitCodes.cs
@@ -2,8 +2,11 @@
 
 internal class ExitCodes
 {
+    internal static readonly int UpdatePushed = 5;
+
     internal static readonly int NoToken = 8;
     internal static readonly int FailedDiscordLogin = 9;
-    internal static readonly int FailedDatabaseLogin = 19;
+
     internal static readonly int FailedDatabaseLoad = 18;
+    internal static readonly int FailedDatabaseLogin = 19;
 }
