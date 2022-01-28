1f2b334a528ef5589be8ed6c403dd0dd2c168ee0
XorogVEVO <xorog@outlook.de>
2022-01-28 03:45:47 +0100

    Created user structure

---
+++ Project-Ichigo/Objects/Users.cs
@@ -0,0 +1,16 @@
+﻿namespace Project_Ichigo.Objects;
+internal class Users
+{
+    internal class Info
+    {
+        public ulong Id { get; set; } = 0;
+        public UrlSubmissions UrlSubmissions { get; set; } = new();
+    }
+
+    internal class UrlSubmissions
+    {
+        public bool UrlSubmissionsIsBanned { get; set; } = false;
+        public DateTime UrlSubmissionsLastTime { get; set; } = DateTime.MinValue;
+        public List<string> UrlSubmissionsSubmittedUrls { get; set; } = new();
+    }
+}
