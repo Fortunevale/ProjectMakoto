0511b06f7bf781cc4deef71576f364db7255fa1f
XorogVEVO <xorog@outlook.de>
2022-02-04 06:54:24 +0100

    Added Phishing Settings

--- Project-Ichigo/Objects/Settings.cs
+++ Project-Ichigo/Objects/Settings.cs
@@ -8,5 +8,21 @@ internal class Settings
     {
         public ulong ServerId { get; set; } = 0;
         public ulong MemberRoleId { get; set; } = 0;
+        public PhishingDetectionSettings PhishingDetectionSettings { get; set; } = new();
+    }
+
+    public class PhishingDetectionSettings
+    {
+        public PhishingPunishmentType PunishmentType { get; set; } = PhishingPunishmentType.BAN;
+        public string CustomPunishmentReason { get; set; } = "";
+        public TimeSpan CustomPunishmentLength { get; set; } = TimeSpan.FromDays(14);
+    }
+
+    public enum PhishingPunishmentType
+    {
+        DELETE,
+        TIMEOUT,
+        KICK,
+        BAN
     }
 }
