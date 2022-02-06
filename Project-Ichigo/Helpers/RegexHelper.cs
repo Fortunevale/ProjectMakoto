9e144d83a1e75b4f85a9ff1e59d209b0b69529e9
XorogVEVO <xorog@outlook.de>
2022-02-06 13:24:25 +0100

    Added RegexList

---
+++ Project-Ichigo/Helpers/RegexHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace Project_Ichigo.Helpers;
+
+internal class RegexHelper
+{
+    internal static string YouTubeUrlRegex = @"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$";
+    internal static string UrlRegex = @"((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:\/~\+#]*[\w\-\@?^=%&amp;\/~\+#])?)";
+}
