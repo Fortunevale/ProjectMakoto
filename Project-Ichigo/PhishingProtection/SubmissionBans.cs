8cce7a31cd3455378d3206d3ee169b23ce11c18e
XorogVEVO <xorog@outlook.de>
2022-02-07 02:36:29 +0100

    Added submissions bans

---
+++ Project-Ichigo/PhishingProtection/SubmissionBans.cs
@@ -0,0 +1,13 @@
+﻿namespace Project_Ichigo.PhishingProtection;
+
+internal class SubmissionBans
+{
+    public Dictionary<string, BanInfo> BannedUsers = new();
+    public Dictionary<string, BanInfo> BannedGuilds = new();
+
+    public class BanInfo
+    {
+        public string Reason { get; set; }
+        public string Moderator { get; set; }
+    }
+}
