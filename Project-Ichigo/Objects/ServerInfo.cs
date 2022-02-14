a49ba139b1901cc84d261cca623eee361cb830f7
XorogVEVO <xorog@outlook.de>
2022-02-14 16:36:28 +0100

    Added BumpReminderSettings

--- Project-Ichigo/Objects/ServerInfo.cs
+++ Project-Ichigo/Objects/ServerInfo.cs
@@ -7,6 +7,37 @@ internal class ServerInfo
     internal class ServerSettings
     {
         public PhishingDetectionSettings PhishingDetectionSettings { get; set; } = new();
+        public BumpReminderSettings BumpReminderSettings { get; set; } = new();
+    }
+
+    public class BumpReminderSettings
+    {
+        private bool _Enabled { get; set; } = false;
+        public bool Enabled { get => _Enabled; set { _Enabled = value; _ = Bot._databaseHelper.SyncDatabase(); } }
+
+
+        private ulong _RoleId { get; set; } = 0;
+        public ulong RoleId { get => _RoleId; set { _RoleId = value; _ = Bot._databaseHelper.SyncDatabase(); } }
+
+
+        private ulong _ChannelId { get; set; } = 0;
+        public ulong ChannelId { get => _ChannelId; set { _ChannelId = value; _ = Bot._databaseHelper.SyncDatabase(); } }
+
+
+        private ulong _MessageId { get; set; } = 0;
+        public ulong MessageId { get => _MessageId; set { _MessageId = value; _ = Bot._databaseHelper.SyncDatabase(); } }
+
+
+        private ulong _LastUserId { get; set; } = 0;
+        public ulong LastUserId { get => _LastUserId; set { _LastUserId = value; _ = Bot._databaseHelper.SyncDatabase(); } }
+
+
+        private DateTime _LastBump { get; set; } = DateTime.MinValue;
+        public DateTime LastBump { get => _LastBump; set { _LastBump = value; _ = Bot._databaseHelper.SyncDatabase(); } }
+
+
+        private DateTime _LastReminder { get; set; } = DateTime.MinValue;
+        public DateTime LastReminder { get => _LastReminder; set { _LastReminder = value; _ = Bot._databaseHelper.SyncDatabase(); } }
     }
 
     public class PhishingDetectionSettings
