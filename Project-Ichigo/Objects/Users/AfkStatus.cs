6dfdf933c8f1060bcd86a71690dccec4427dd7dc
XorogVEVO <xorog@outlook.de>
2022-02-22 14:22:55 +0100

    blub

--- Project-Ichigo/Objects/Users/AfkStatus.cs
+++ Project-Ichigo/Objects/Users/AfkStatus.cs
@@ -4,6 +4,8 @@ internal class AfkStatus
     private string _Reason { get; set; } = "";
     public string Reason { get => _Reason; set { _Reason = value; _ = Bot._databaseHelper.SyncDatabase(); } }
     
+
+
     private DateTime _TimeStamp { get; set; } = DateTime.UnixEpoch;
     public DateTime TimeStamp { get => _TimeStamp; set { _TimeStamp = value; _ = Bot._databaseHelper.SyncDatabase(); } }
 
