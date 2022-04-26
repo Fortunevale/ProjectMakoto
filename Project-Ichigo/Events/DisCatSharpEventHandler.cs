a6306e35084ec90d64e5ee9c0016ee3138b3cbde
TheXorog <56395159+TheXorog@users.noreply.github.com>
2022-04-26 12:22:58 +0200

    [FIX] Validate Database for executing events

--- Project-Ichigo/Events/DisCatSharpEventHandler.cs
+++ Project-Ichigo/Events/DisCatSharpEventHandler.cs
@@ -36,10 +36,38 @@ internal class DisCatSharpEventHandler
     ExperienceEvents experienceEvents { get; set; }
     ReactionRoleEvents reactionRoleEvents { get; set; }
 
+    internal void FillDatabase(DiscordGuild guild = null, DiscordMember member = null, DiscordUser user = null)
+    {
+        if (guild is not null)
+            if (!_bot._guilds.List.ContainsKey(guild.Id))
+                _bot._guilds.List.Add(guild.Id, new Guilds.ServerSettings());
 
+        if (guild.Members is not null && guild.Members.Count > 0)
+            foreach(var b in guild.Members)
+                if (!_bot._guilds.List[ guild.Id ].Members.ContainsKey(b.Key))
+                    _bot._guilds.List[ guild.Id ].Members.Add(b.Key, new());
+
+        if (member is not null && guild is not null)
+            if (!_bot._guilds.List[ guild.Id ].Members.ContainsKey(member.Id))
+                _bot._guilds.List[ guild.Id ].Members.Add(member.Id, new());
+
+        if (user is not null && guild is not null)
+            if (!_bot._guilds.List[ guild.Id ].Members.ContainsKey(user.Id))
+                _bot._guilds.List[ guild.Id ].Members.Add(user.Id, new());
+
+        if (member is not null)
+            if (!_bot._users.List.ContainsKey(member.Id))
+                _bot._users.List.Add(member.Id, new());
+
+        if (user is not null)
+            if (!_bot._users.List.ContainsKey(user.Id))
+                _bot._users.List.Add(user.Id, new());
+    }
 
     internal async Task GuildMemberAdded(DiscordClient sender, GuildMemberAddEventArgs e)
     {
+        FillDatabase(e.Guild, e.Member);
+
         _ = genericGuildEvents.GuildMemberAdded(sender, e);
         _ = actionlogEvents.UserJoined(sender, e);
         _ = joinEvents.GuildMemberAdded(sender, e);
@@ -47,6 +75,8 @@ internal class DisCatSharpEventHandler
 
     internal async Task GuildMemberRemoved(DiscordClient sender, GuildMemberRemoveEventArgs e)
     {
+        FillDatabase(e.Guild, e.Member);
+
         _ = genericGuildEvents.GuildMemberRemoved(sender, e);
         _ = actionlogEvents.UserLeft(sender, e);
         _ = joinEvents.GuildMemberRemoved(sender, e);
@@ -54,28 +84,37 @@ internal class DisCatSharpEventHandler
 
     internal async Task GuildMemberUpdated(DiscordClient sender, GuildMemberUpdateEventArgs e)
     {
+        FillDatabase(e.Guild, e.Member);
+
         _ = genericGuildEvents.GuildMemberUpdated(sender, e);
         _ = actionlogEvents.MemberUpdated(sender, e);
     }
 
     internal async Task GuildBanAdded(DiscordClient sender, GuildBanAddEventArgs e)
     {
+        FillDatabase(e.Guild, e.Member);
+
         _ = genericGuildEvents.GuildMemberBanned(sender, e);
         _ = actionlogEvents.BanAdded(sender, e);
     }
 
     internal async Task CommandExecuted(CommandsNextExtension sender, CommandExecutionEventArgs e)
     {
+        FillDatabase(e.Context.Guild, e.Context.Member);
+
         _ = commandEvents.CommandExecuted(sender, e);
     }
 
     internal async Task CommandError(CommandsNextExtension sender, CommandErrorEventArgs e)
     {
+        FillDatabase(e.Context.Guild, e.Context.Member);
         _ = commandEvents.CommandError(sender, e);
     }
 
     internal async Task MessageCreated(DiscordClient sender, MessageCreateEventArgs e)
     {
+        FillDatabase(e.Guild, user: e.Message.Author);
+
         _ = afkEvents.MessageCreated(sender, e);
         _ = crosspostEvents.MessageCreated(sender, e);
         _ = phishingProtectionEvents.MessageCreated(sender, e);
@@ -85,89 +124,123 @@ internal class DisCatSharpEventHandler
 
     internal async Task MessageUpdated(DiscordClient sender, MessageUpdateEventArgs e)
     {
+        FillDatabase(e.Guild, user: e.Message.Author);
+
         _ = phishingProtectionEvents.MessageUpdated(sender, e);
         _ = actionlogEvents.MessageUpdated(sender, e);
     }
 
     internal async Task ComponentInteractionCreated(DiscordClient sender, ComponentInteractionCreateEventArgs e)
     {
+        FillDatabase(e.Guild, user: e.Interaction.User);
+
         _ = submissionEvents.ComponentInteractionCreated(sender, e);
     }
 
     internal async Task GuildCreated(DiscordClient sender, GuildCreateEventArgs e)
     {
+        FillDatabase(e.Guild);
+
         _ = discordEvents.GuildCreated(sender, e);
     }
 
     internal async Task MessageDeleted(DiscordClient sender, MessageDeleteEventArgs e)
     {
+        FillDatabase(e.Guild, user: e.Message.Author);
+
         _ = actionlogEvents.MessageDeleted(sender, e);
         _ = bumpReminderEvents.MessageDeleted(sender, e);
     }
 
     internal async Task MessagesBulkDeleted(DiscordClient sender, MessageBulkDeleteEventArgs e)
     {
+        FillDatabase(e.Guild);
+
         _ = actionlogEvents.MessageBulkDeleted(sender, e);
     }
 
     internal async Task GuildRoleCreated(DiscordClient sender, GuildRoleCreateEventArgs e)
     {
+        FillDatabase(e.Guild);
+
         _ = actionlogEvents.RoleCreated(sender, e);
     }
 
     internal async Task GuildRoleUpdated(DiscordClient sender, GuildRoleUpdateEventArgs e)
     {
+        FillDatabase(e.Guild);
+
         _ = actionlogEvents.RoleModified(sender, e);
     }
 
     internal async Task GuildRoleDeleted(DiscordClient sender, GuildRoleDeleteEventArgs e)
     {
+        FillDatabase(e.Guild);
+
         _ = actionlogEvents.RoleDeleted(sender, e);
     }
 
     internal async Task GuildBanRemoved(DiscordClient sender, GuildBanRemoveEventArgs e)
     {
+        FillDatabase(e.Guild, e.Member);
+
         _ = actionlogEvents.BanRemoved(sender, e);
     }
 
     internal async Task GuildUpdated(DiscordClient sender, GuildUpdateEventArgs e)
     {
+        FillDatabase(e.GuildAfter);
+
         _ = actionlogEvents.GuildUpdated(sender, e);
     }
 
     internal async Task ChannelCreated(DiscordClient sender, ChannelCreateEventArgs e)
     {
+        FillDatabase(e.Guild);
+
         _ = actionlogEvents.ChannelCreated(sender, e);
     }
 
     internal async Task ChannelDeleted(DiscordClient sender, ChannelDeleteEventArgs e)
     {
+        FillDatabase(e.Guild);
+
         _ = actionlogEvents.ChannelDeleted(sender, e);
     }
 
     internal async Task ChannelUpdated(DiscordClient sender, ChannelUpdateEventArgs e)
     {
+        FillDatabase(e.Guild);
+
         _ = actionlogEvents.ChannelUpdated(sender, e);
     }
 
     internal async Task InviteCreated(DiscordClient sender, InviteCreateEventArgs e)
     {
+        FillDatabase(e.Guild);
+
         _ = actionlogEvents.InviteCreated(sender, e);
     }
 
     internal async Task InviteDeleted(DiscordClient sender, InviteDeleteEventArgs e)
     {
+        FillDatabase(e.Guild);
+
         _ = actionlogEvents.InviteDeleted(sender, e);
     }
 
     internal async Task MessageReactionAdded(DiscordClient sender, MessageReactionAddEventArgs e)
     {
+        FillDatabase(e.Guild, user: e.User);
+
         _ = bumpReminderEvents.ReactionAdded(sender, e);
         _ = reactionRoleEvents.MessageReactionAdded(sender, e);
     }
 
     internal async Task MessageReactionRemoved(DiscordClient sender, MessageReactionRemoveEventArgs e)
     {
+        FillDatabase(e.Guild, user: e.User);
+
         _ = bumpReminderEvents.ReactionRemoved(sender, e);
         _ = reactionRoleEvents.MessageReactionRemoved(sender, e);
     }
