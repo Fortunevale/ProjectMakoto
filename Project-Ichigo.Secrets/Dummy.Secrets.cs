07c6b2dcf9437f48ec2f56a6b55dd3fda0ae1a91
TheXorog <56395159+TheXorog@users.noreply.github.com>
2022-05-05 19:08:32 +0200

    [REF] Moved Secrets to separate Project

---
+++ Project-Ichigo.Secrets/Dummy.Secrets.cs
@@ -0,0 +1,31 @@
+﻿namespace Project_Ichigo.Secrets.Dummy;
+
+// This is a dummy file. When actually deploying, please remove "Dummy." at the beginning of the file name and fill in information.
+
+internal class Secrets
+{
+    internal static readonly protected string LavalinkUrl = "";
+    internal static readonly protected int LavalinkPort = 0;
+    internal static readonly protected string LavalinkPassword = "";
+
+
+    internal static readonly protected string DatabaseUrl = "";
+    internal static readonly protected int DatabasePort = 0;
+#if DEBUG
+    internal static readonly protected string MainDatabaseName = "";
+#endif
+#if !DEBUG
+    internal static readonly protected string MainDatabaseName = "";
+#endif
+#if DEBUG
+    internal static readonly protected string GuildDatabaseName = "";
+#endif
+#if !DEBUG
+    internal static readonly protected string GuildDatabaseName = "";
+#endif
+    internal static readonly protected string DatabaseUserName = "";
+    internal static readonly protected string DatabasePassword = "";
+
+    internal static readonly protected string GithubToken = "";
+    internal static readonly protected DateTime GithubTokenExperiation = new(2000, 01, 01, 01, 00, 00, DateTimeKind.Utc);
+}
