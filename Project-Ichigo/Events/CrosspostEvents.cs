003cda80ad44da8203f7b12f2bdd4d3fe1a28a1c
Mira <56395159+TheXorog@users.noreply.github.com>
2022-06-08 01:27:35 +0200

    Save Crosspost Messages and re-queue in restart
    Close #53

--- Project-Ichigo/Events/CrosspostEvents.cs
+++ Project-Ichigo/Events/CrosspostEvents.cs
@@ -23,52 +23,66 @@ internal class CrosspostEvents
                 if (!e.Guild.Channels.ContainsKey(b))
                     _bot._guilds.List[e.Guild.Id].CrosspostSettings.CrosspostChannels.Remove(b);
 
-            if (_bot._guilds.List[e.Guild.Id].CrosspostSettings.CrosspostChannels.Contains(e.Channel.Id))
+            if (!_bot._guilds.List[e.Guild.Id].CrosspostSettings.CrosspostChannels.Contains(e.Channel.Id))
+                return;
+
+            if (e.Channel.Type == ChannelType.News)
             {
-                if (e.Channel.Type == ChannelType.News)
-                {
-                    if (_bot._guilds.List[e.Guild.Id].CrosspostSettings.ExcludeBots)
-                        if (e.Message.WebhookMessage || e.Message.Author.IsBot)
-                            return;
-
-                    if (_bot._guilds.List[e.Guild.Id].CrosspostSettings.DelayBeforePosting > 3)
-                        _ = e.Message.CreateReactionAsync(DiscordEmoji.FromUnicode("🕒"));
-
-                    await Task.Delay(TimeSpan.FromSeconds(_bot._guilds.List[e.Guild.Id].CrosspostSettings.DelayBeforePosting));
-
-                    if (_bot._guilds.List[e.Guild.Id].CrosspostSettings.DelayBeforePosting > 3)
-                        _ = e.Message.DeleteOwnReactionAsync(DiscordEmoji.FromUnicode("🕒"));
-
-                    DiscordMessage msg;
-
-                    try
-                    {
-                        msg = await e.Channel.GetMessageAsync(e.Message.Id);
-                    }
-                    catch (DisCatSharp.Exceptions.NotFoundException)
-                    {
+                if (_bot._guilds.List[e.Guild.Id].CrosspostSettings.ExcludeBots)
+                    if (e.Message.WebhookMessage || e.Message.Author.IsBot)
                         return;
-                    }
-                    catch (Exception)
+
+                ulong MessageId = e.Message.Id;
+
+                _bot._guilds.List[e.Guild.Id].CrosspostSettings.CrosspostTasks.Add(new CrosspostMessage
+                {
+                    MessageId = e.Message.Id,
+                    ChannelId = e.Channel.Id
+                });
+
+                if (_bot._guilds.List[e.Guild.Id].CrosspostSettings.DelayBeforePosting > 3)
+                    _ = e.Message.CreateReactionAsync(DiscordEmoji.FromUnicode("🕒"));
+
+                await Task.Delay(TimeSpan.FromSeconds(_bot._guilds.List[e.Guild.Id].CrosspostSettings.DelayBeforePosting));
+
+                if (_bot._guilds.List[e.Guild.Id].CrosspostSettings.DelayBeforePosting > 3)
+                    _ = e.Message.DeleteOwnReactionAsync(DiscordEmoji.FromUnicode("🕒"));
+
+                DiscordMessage msg;
+
+                try
+                {
+                    msg = await e.Channel.GetMessageAsync(e.Message.Id);
+                }
+                catch (DisCatSharp.Exceptions.NotFoundException)
+                {
+                    return;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                bool ReactionAdded = false;
+
+                var task = e.Channel.CrosspostMessageAsync(msg).ContinueWith(s =>
+                {
+                    if (_bot._guilds.List[e.Guild.Id].CrosspostSettings.CrosspostTasks.Any(x => x.MessageId == MessageId))
                     {
-                        throw;
+                        var obj = _bot._guilds.List[e.Guild.Id].CrosspostSettings.CrosspostTasks.First(x => x.MessageId == MessageId);
+                        _bot._guilds.List[e.Guild.Id].CrosspostSettings.CrosspostTasks.Remove(obj);
                     }
 
-                    bool ReactionAdded = false;
+                    if (ReactionAdded)
+                        _ = msg.DeleteOwnReactionAsync(DiscordEmoji.FromGuildEmote(sender, 974029756355977216));
+                });
 
-                    var task = e.Channel.CrosspostMessageAsync(msg).ContinueWith(s =>
-                    {
-                        if (ReactionAdded)
-                            _ = msg.DeleteOwnReactionAsync(DiscordEmoji.FromGuildEmote(sender, 974029756355977216));
-                    });
+                await Task.Delay(5000);
 
-                    await Task.Delay(5000);
-
-                    if (!task.IsCompleted)
-                    {
-                        await msg.CreateReactionAsync(DiscordEmoji.FromGuildEmote(sender, 974029756355977216));
-                        ReactionAdded = true;
-                    }
+                if (!task.IsCompleted)
+                {
+                    await msg.CreateReactionAsync(DiscordEmoji.FromGuildEmote(sender, 974029756355977216));
+                    ReactionAdded = true;
                 }
             }
         }).Add(_bot._watcher);
