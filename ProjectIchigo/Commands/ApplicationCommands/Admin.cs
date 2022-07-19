9161fa37973cb2bf799f96d880ab11e06bca31a2
Mira <56395159+TheXorog@users.noreply.github.com>
2022-07-19 08:23:00 +0200

    Port LevelRewards Command

--- ProjectIchigo/Commands/ApplicationCommands/Admin.cs
+++ ProjectIchigo/Commands/ApplicationCommands/Admin.cs
@@ -4,7 +4,7 @@ internal class Admin : ApplicationCommandsModule
     public Bot _bot { private get; set; }
 
     [SlashCommandGroup("join", "Allows to review and change settings in the event somebody joins the server")]
-    public class JoinCommand : ApplicationCommandsModule
+    public class Join : ApplicationCommandsModule
     {
         public Bot _bot { private get; set; }
 
@@ -28,7 +28,7 @@ internal class Admin : ApplicationCommandsModule
     }
     
     [SlashCommandGroup("experience", "Allows to review and change settings related to experience")]
-    public class ExperienceCommand : ApplicationCommandsModule
+    public class Experience : ApplicationCommandsModule
     {
         public Bot _bot { private get; set; }
 
@@ -47,6 +47,30 @@ internal class Admin : ApplicationCommandsModule
             Task.Run(async () =>
             {
                 await new Commands.ExperienceCommand.ConfigCommand().ExecuteCommand(ctx, _bot);
+            }).Add(_bot._watcher, ctx);
+        }
+    }
+    
+    [SlashCommandGroup("levelrewards", "Allows to review, add, remove and modify Level Rewards")]
+    public class LevelRewards : ApplicationCommandsModule
+    {
+        public Bot _bot { private get; set; }
+
+        [SlashCommand("review", "Shows a list of all currently defined Level Rewards")]
+        public async Task Review(InteractionContext ctx)
+        {
+            Task.Run(async () =>
+            {
+                await new Commands.LevelRewardsCommand.ReviewCommand().ExecuteCommand(ctx, _bot);
+            }).Add(_bot._watcher, ctx);
+        }
+
+        [SlashCommand("config", "Allows adding, removing and modifying currently defined Level Rewards")]
+        public async Task Config(InteractionContext ctx)
+        {
+            Task.Run(async () =>
+            {
+                await new Commands.LevelRewardsCommand.ConfigCommand().ExecuteCommand(ctx, _bot);
             }).Add(_bot._watcher, ctx);
         }
     }
