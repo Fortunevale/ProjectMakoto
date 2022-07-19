c2342fe7ce0445239c54f81584014442226ac404
Mira <56395159+TheXorog@users.noreply.github.com>
2022-07-19 07:57:26 +0200

    Port Generic Selectors to new Command System

--- ProjectIchigo/Commands/Commands/BaseCommand.cs
+++ ProjectIchigo/Commands/Commands/BaseCommand.cs
@@ -208,6 +208,336 @@ internal abstract class BaseCommand
         throw new NotImplementedException();
     }
 
+    internal async Task<DiscordRole> PromptRoleSelection(bool IncludeCreateForMe = false, string CreateForMeName = "Role", bool IncludeDisable = false, string DisableString = "Disable")
+    {
+        List<DiscordSelectComponentOption> roles = new();
+
+        if (IncludeCreateForMe)
+            roles.Add(new DiscordSelectComponentOption($"Create one for me..", "create_for_me", "", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("➕"))));
+
+        if (IncludeDisable)
+            roles.Add(new DiscordSelectComponentOption(DisableString, "disable", "", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("❌"))));
+
+        foreach (var role in Context.Guild.Roles.OrderByDescending(x => x.Value.Position))
+        {
+            if (Context.CurrentMember.GetHighestPosition() > role.Value.Position && Context.Member.GetHighestPosition() > role.Value.Position && !role.Value.IsManaged && role.Value.Id != Context.Guild.EveryoneRole.Id)
+                roles.Add(new DiscordSelectComponentOption($"@{role.Value.Name} ({role.Value.Id})", role.Value.Id.ToString(), "", false, new DiscordComponentEmoji(role.Value.Color.GetClosestColorEmoji(Context.Client))));
+        }
+
+        int currentPage = 0;
+        string SelectionInteractionId = Guid.NewGuid().ToString();
+        string NextPageId = Guid.NewGuid().ToString();
+        string PrevPageId = Guid.NewGuid().ToString();
+
+        DiscordRole Role = null;
+
+        bool FinishedSelection = false;
+        bool ExceptionOccured = false;
+        Exception exception = null;
+
+        async Task RefreshRoleList()
+        {
+            var previousPageButton = new DiscordButtonComponent(ButtonStyle.Primary, PrevPageId, "Previous page", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("◀")));
+            var nextPageButton = new DiscordButtonComponent(ButtonStyle.Primary, NextPageId, "Next page", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("▶")));
+
+            var dropdown = new DiscordSelectComponent("Select a role..", roles.Skip(currentPage * 25).Take(25), SelectionInteractionId);
+            var builder = new DiscordMessageBuilder().WithEmbed(new DiscordEmbedBuilder(Context.ResponseMessage.Embeds[0]).WithColor(EmbedColors.AwaitingInput)).AddComponents(dropdown).WithContent(Context.ResponseMessage.Content);
+
+            if (roles.Skip(currentPage * 25).Count() > 25)
+                builder.AddComponents(nextPageButton);
+
+            if (currentPage != 0)
+                builder.AddComponents(previousPageButton);
+
+            await RespondOrEdit(builder);
+        }
+
+        _ = RefreshRoleList();
+
+        int TimeoutSeconds = 60;
+
+        async Task RunDropdownInteraction(DiscordClient s, ComponentInteractionCreateEventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                try
+                {
+                    if (e.Message?.Id == Context.ResponseMessage.Id && e.User.Id == Context.User.Id)
+                    {
+                        TimeoutSeconds = 60;
+                        _ = e.Interaction.CreateResponseAsync(InteractionResponseType.DeferredMessageUpdate);
+
+                        if (e.Interaction.Data.CustomId == SelectionInteractionId)
+                        {
+                            Context.Client.ComponentInteractionCreated -= RunDropdownInteraction;
+
+                            if (e.Values.First() is "create_for_me")
+                                Role = await Context.Guild.CreateRoleAsync(CreateForMeName);
+                            else if (e.Values.First() is "disable")
+                                Role = null;
+                            else
+                                Role = Context.Guild.GetRole(Convert.ToUInt64(e.Values.First()));
+
+
+                            FinishedSelection = true;
+                        }
+                        else if (e.Interaction.Data.CustomId == PrevPageId)
+                        {
+                            currentPage--;
+                            await RefreshRoleList();
+                        }
+                        else if (e.Interaction.Data.CustomId == NextPageId)
+                        {
+                            currentPage++;
+                            await RefreshRoleList();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    exception = ex;
+                    ExceptionOccured = true;
+                    FinishedSelection = true;
+                    throw;
+                }
+            }).Add(Context.Bot._watcher, Context);
+        }
+
+        Context.Client.ComponentInteractionCreated += RunDropdownInteraction;
+
+        while (!FinishedSelection && TimeoutSeconds >= 0)
+        {
+            await Task.Delay(1000);
+            TimeoutSeconds--;
+        }
+
+        Context.Client.ComponentInteractionCreated -= RunDropdownInteraction;
+
+        await RespondOrEdit(new DiscordMessageBuilder().WithEmbed(Context.ResponseMessage.Embeds[0]).WithContent(Context.ResponseMessage.Content));
+
+        if (ExceptionOccured)
+            throw exception;
+
+        if (TimeoutSeconds <= 0)
+            throw new ArgumentException("No selection made");
+
+        return Role;
+    }
+
+    internal async Task<DiscordChannel> PromptChannelSelection(bool IncludeCreateForMe = false, string CreateForMeName = "Channel", ChannelType CreateFormeChannelType = ChannelType.Text, bool IncludeDisable = false, string DisableString = "Disable")
+    {
+        List<DiscordSelectComponentOption> channels = new();
+
+        if (IncludeCreateForMe)
+            channels.Add(new DiscordSelectComponentOption($"Create one for me..", "create_for_me", "", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("➕"))));
+
+        if (IncludeDisable)
+            channels.Add(new DiscordSelectComponentOption(DisableString, "disable", "", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("❌"))));
+
+        foreach (var category in await Context.Guild.GetOrderedChannelsAsync())
+        {
+            foreach (var b in category.Value)
+                channels.Add(new DiscordSelectComponentOption(
+                    $"#{b.Name} ({b.Id})",
+                    b.Id.ToString(),
+                    $"{(category.Key != 0 ? $"{b.Parent.Name} " : "")}"));
+        }
+
+        int currentPage = 0;
+        string SelectionInteractionId = Guid.NewGuid().ToString();
+        string NextPageId = Guid.NewGuid().ToString();
+        string PrevPageId = Guid.NewGuid().ToString();
+
+        DiscordChannel Channel = null;
+
+        bool FinishedSelection = false;
+        bool ExceptionOccured = false;
+        Exception exception = null;
+
+        async Task RefreshRoleList()
+        {
+            var previousPageButton = new DiscordButtonComponent(ButtonStyle.Primary, PrevPageId, "Previous page", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("◀")));
+            var nextPageButton = new DiscordButtonComponent(ButtonStyle.Primary, NextPageId, "Next page", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("▶")));
+
+            var dropdown = new DiscordSelectComponent("Select a channel..", channels.Skip(currentPage * 25).Take(25) as IEnumerable<DiscordSelectComponentOption>, SelectionInteractionId);
+            var builder = new DiscordMessageBuilder().WithEmbed(new DiscordEmbedBuilder(Context.ResponseMessage.Embeds[0]).WithColor(EmbedColors.AwaitingInput)).AddComponents(dropdown).WithContent(Context.ResponseMessage.Content);
+
+            if (channels.Skip(currentPage * 25).Count() > 25)
+                builder.AddComponents(nextPageButton);
+
+            if (currentPage != 0)
+                builder.AddComponents(previousPageButton);
+
+            await RespondOrEdit(builder);
+        }
+
+        _ = RefreshRoleList();
+
+        int TimeoutSeconds = 60;
+
+        async Task RunDropdownInteraction(DiscordClient s, ComponentInteractionCreateEventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                try
+                {
+                    if (e.Message?.Id == Context.ResponseMessage.Id && e.User.Id == Context.User.Id)
+                    {
+                        TimeoutSeconds = 60;
+                        _ = e.Interaction.CreateResponseAsync(InteractionResponseType.DeferredMessageUpdate);
+
+                        if (e.Interaction.Data.CustomId == SelectionInteractionId)
+                        {
+                            Context.Client.ComponentInteractionCreated -= RunDropdownInteraction;
+
+                            if (e.Values.First() is "create_for_me")
+                                Channel = await Context.Guild.CreateChannelAsync(CreateForMeName, CreateFormeChannelType);
+                            else if (e.Values.First() is "disable")
+                                Channel = null;
+                            else
+                                Channel = Context.Guild.GetChannel(Convert.ToUInt64(e.Values.First()));
+
+                            FinishedSelection = true;
+                        }
+                        else if (e.Interaction.Data.CustomId == PrevPageId)
+                        {
+                            currentPage--;
+                            await RefreshRoleList();
+                        }
+                        else if (e.Interaction.Data.CustomId == NextPageId)
+                        {
+                            currentPage++;
+                            await RefreshRoleList();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    exception = ex;
+                    ExceptionOccured = true;
+                    FinishedSelection = true;
+                    throw;
+                }
+            }).Add(Context.Bot._watcher, Context);
+        }
+
+        Context.Client.ComponentInteractionCreated += RunDropdownInteraction;
+
+        while (!FinishedSelection && TimeoutSeconds >= 0)
+        {
+            await Task.Delay(1000);
+            TimeoutSeconds--;
+        }
+
+        Context.Client.ComponentInteractionCreated -= RunDropdownInteraction;
+
+        await RespondOrEdit(new DiscordMessageBuilder().WithEmbed(Context.ResponseMessage.Embeds[0]).WithContent(Context.ResponseMessage.Content));
+
+        if (ExceptionOccured)
+            throw exception;
+
+        if (TimeoutSeconds <= 0)
+            throw new ArgumentException("No selection made");
+
+        return Channel;
+    }
+
+    internal async Task<string> PromptCustomSelection(List<DiscordSelectComponentOption> options, string CustomPlaceHolder = "Select an option..")
+    {
+        int currentPage = 0;
+        string SelectionInteractionId = Guid.NewGuid().ToString();
+        string NextPageId = Guid.NewGuid().ToString();
+        string PrevPageId = Guid.NewGuid().ToString();
+
+        string Selection = null;
+
+        bool FinishedSelection = false;
+        bool ExceptionOccured = false;
+        Exception exception = null;
+
+        async Task Refresh()
+        {
+            var previousPageButton = new DiscordButtonComponent(ButtonStyle.Primary, PrevPageId, "Previous page", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("◀")));
+            var nextPageButton = new DiscordButtonComponent(ButtonStyle.Primary, NextPageId, "Next page", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("▶")));
+
+            var dropdown = new DiscordSelectComponent(CustomPlaceHolder, options.Skip(currentPage * 25).Take(25) as IEnumerable<DiscordSelectComponentOption>, SelectionInteractionId);
+            var builder = new DiscordMessageBuilder().WithEmbed(new DiscordEmbedBuilder(Context.ResponseMessage.Embeds[0]).WithColor(EmbedColors.AwaitingInput)).AddComponents(dropdown).WithContent(Context.ResponseMessage.Content);
+
+            if (options.Skip(currentPage * 25).Count() > 25)
+                builder.AddComponents(nextPageButton);
+
+            if (currentPage != 0)
+                builder.AddComponents(previousPageButton);
+
+            await RespondOrEdit(builder);
+        }
+
+        _ = Refresh();
+
+        int TimeoutSeconds = 60;
+
+        async Task RunDropdownInteraction(DiscordClient s, ComponentInteractionCreateEventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                try
+                {
+                    if (e.Message?.Id == Context.ResponseMessage.Id && e.User.Id == Context.User.Id)
+                    {
+                        TimeoutSeconds = 60;
+                        _ = e.Interaction.CreateResponseAsync(InteractionResponseType.DeferredMessageUpdate);
+
+                        if (e.Interaction.Data.CustomId == SelectionInteractionId)
+                        {
+                            Context.Client.ComponentInteractionCreated -= RunDropdownInteraction;
+
+                            Selection = e.Values.First();
+
+                            FinishedSelection = true;
+                        }
+                        else if (e.Interaction.Data.CustomId == PrevPageId)
+                        {
+                            currentPage--;
+                            await Refresh();
+                        }
+                        else if (e.Interaction.Data.CustomId == NextPageId)
+                        {
+                            currentPage++;
+                            await Refresh();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    exception = ex;
+                    ExceptionOccured = true;
+                    FinishedSelection = true;
+                    throw;
+                }
+            }).Add(Context.Bot._watcher, Context);
+        }
+
+        Context.Client.ComponentInteractionCreated += RunDropdownInteraction;
+
+        while (!FinishedSelection && TimeoutSeconds >= 0)
+        {
+            await Task.Delay(1000);
+            TimeoutSeconds--;
+        }
+
+        Context.Client.ComponentInteractionCreated -= RunDropdownInteraction;
+
+        await RespondOrEdit(new DiscordMessageBuilder().WithEmbed(Context.ResponseMessage.Embeds[0]).WithContent(Context.ResponseMessage.Content));
+
+        if (ExceptionOccured)
+            throw exception;
+
+        if (TimeoutSeconds <= 0)
+            throw new ArgumentException("No selection made");
+
+        return Selection;
+    }
+
     public void ModifyToTimedOut(bool Delete = false)
     {
         _ = RespondOrEdit(new DiscordMessageBuilder().WithEmbed(new DiscordEmbedBuilder(Context.ResponseMessage.Embeds[0]).WithFooter(Context.ResponseMessage.Embeds[0].Footer.Text + " • Interaction timed out")));
