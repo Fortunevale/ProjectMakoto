5eb586f11ecde0ca4276b2a8278b8f69d78c89fe
Mira <56395159+TheXorog@users.noreply.github.com>
2022-09-10 07:47:44 +0200

    Add Automatic Token Detection Settings

---
+++ ProjectIchigo/Commands/Commands/Configuration/TokenDetectionCommand/ConfigCommand.cs
@@ -0,0 +1,52 @@
+﻿namespace ProjectIchigo.Commands.TokenDetectionCommand;
+
+internal class ConfigCommand : BaseCommand
+{
+    public override async Task<bool> BeforeExecution(SharedCommandContext ctx) => await CheckAdmin();
+
+    public override Task ExecuteCommand(SharedCommandContext ctx, Dictionary<string, object> arguments)
+    {
+        return Task.Run(async () =>
+        {
+            if (await ctx.Bot.users[ctx.Member.Id].Cooldown.WaitForLight(ctx.Client, ctx))
+                return;
+
+            var embed = new DiscordEmbedBuilder
+            {
+                Description = TokenDetectionCommandAbstractions.GetCurrentConfiguration(ctx)
+            }.SetAwaitingInput(ctx, "Token Detection");
+
+            var Toggle = new DiscordButtonComponent((ctx.Bot.guilds[ctx.Guild.Id].TokenLeakDetectionSettings.DetectTokens? ButtonStyle.Danger : ButtonStyle.Success), Guid.NewGuid().ToString(), "Toggle Token Detection", false, new DiscordComponentEmoji(DiscordEmoji.FromUnicode("⚠")));
+
+            await RespondOrEdit(new DiscordMessageBuilder().WithEmbed(embed)
+            .AddComponents(new List<DiscordComponent>
+            {
+                Toggle
+            })
+            .AddComponents(MessageComponents.CancelButton));
+
+            var e = await ctx.Client.GetInteractivity().WaitForButtonAsync(ctx.ResponseMessage, ctx.User, TimeSpan.FromMinutes(2));
+
+            if (e.TimedOut)
+            {
+                ModifyToTimedOut(true);
+                return;
+            }
+
+            _ = e.Result.Interaction.CreateResponseAsync(InteractionResponseType.DeferredMessageUpdate);
+
+            if (e.Result.Interaction.Data.CustomId == Toggle.CustomId)
+            {
+                ctx.Bot.guilds[ctx.Guild.Id].TokenLeakDetectionSettings.DetectTokens = !ctx.Bot.guilds[ctx.Guild.Id].TokenLeakDetectionSettings.DetectTokens;
+
+                await ExecuteCommand(ctx, arguments);
+                return;
+            }
+            else if (e.Result.Interaction.Data.CustomId == MessageComponents.CancelButton.CustomId)
+            {
+                DeleteOrInvalidate();
+                return;
+            }
+        });
+    }
+}
---
+++ ProjectIchigo/Commands/Commands/Configuration/TokenDetectionCommand/TokenDetectionCommandAbstractions.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectIchigo.Commands.TokenDetectionCommand;
+
+internal class TokenDetectionCommandAbstractions
+{
+    internal static string GetCurrentConfiguration(SharedCommandContext ctx)
+    {
+        return $"⚠ `Detect Tokens`: {ctx.Bot.guilds[ctx.Guild.Id].TokenLeakDetectionSettings.DetectTokens.BoolToEmote(ctx.Client)}";
+    }
+}
