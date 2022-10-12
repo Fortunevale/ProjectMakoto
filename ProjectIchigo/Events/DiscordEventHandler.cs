c70526781ba8e31b40128a644e4a099fc1c2afa6
Mira <56395159+TheXorog@users.noreply.github.com>
2022-10-12 19:13:41 +0200

    Update DiscordEventHandler.cs

--- ProjectIchigo/Events/DiscordEventHandler.cs
+++ ProjectIchigo/Events/DiscordEventHandler.cs
@@ -162,7 +162,7 @@ internal class DiscordEventHandler
 
             if (!e.Message.Content.IsNullOrWhiteSpace() && (e.Message.Content == $"<@{sender.CurrentUser.Id}>" || e.Message.Content == $"<@!{sender.CurrentUser.Id}>"))
             {
-                _ = e.Message.RespondAsync($"Hi {e.Author.Mention}, i'm Ichigo. My prefix is `;;`. To get help, type `;;help`.\n" +
+                _ = e.Message.RespondAsync($"Hi {e.Author.Mention}, i'm Ichigo. I support Slash Commands, but additionally you can use me via `;;`. To get a list of all commands, type `;;help` or do a `/` and filter by me.\n" +
                                 $"If you need help, feel free to join our Support and Development Server: <{_bot.status.DevelopmentServerInvite}>\n\n" +
                                 $"To find out more about me, check my Github Repo: <https://s.aitsys.dev/ichigo>.");
             }
