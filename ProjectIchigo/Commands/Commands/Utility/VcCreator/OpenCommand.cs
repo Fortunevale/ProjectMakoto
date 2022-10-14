faa618b89fef07804c9d9c907de80e47008260cd
Mira <56395159+TheXorog@users.noreply.github.com>
2022-10-14 20:37:22 +0200

    Create OpenCommand.cs

---
+++ ProjectIchigo/Commands/Commands/Utility/VcCreator/OpenCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectIchigo.Commands.VcCreator;
+
+internal class OpenCommand : BaseCommand
+{
+    public override Task ExecuteCommand(SharedCommandContext ctx, Dictionary<string, object> arguments)
+    {
+        return Task.Run(async () =>
+        {
+
+        });
+    }
+}
