0c5617abb1438af501e3d60578408e3739959f22
Mira <56395159+TheXorog@users.noreply.github.com>
2023-01-06 02:32:35 +0100

    Update TranslationKey.cs

--- ProjectIchigo/Entities/Translation/TranslationKey.cs
+++ ProjectIchigo/Entities/Translation/TranslationKey.cs
@@ -6,21 +6,45 @@ public class TranslationKey
 
     public string Get(User user)
     {
-        if (!user.OverrideLocale.IsNullOrWhiteSpace())
-            if (t.ContainsKey(user?.OverrideLocale ?? "en"))
-                return t[user?.OverrideLocale ?? "en"];
+        string? Locale;
 
-        if (t.ContainsKey(user?.CurrentLocale ?? "en"))
-            return t[user?.CurrentLocale ?? "en"];
+        if (!user.OverrideLocale.IsNullOrWhiteSpace())
+            Locale = user.OverrideLocale;
         else
-            return t["en"];
+            Locale = user.CurrentLocale;
+
+        if (Locale is null && !t.ContainsKey("en"))
+            return "Missing Translation. Please report to the developer.";
+
+        Locale ??= "en";
+
+        return t[Locale];
     }
-    
+
+    public string Get(DiscordGuild guild)
+    {
+        string? Locale = null;
+
+        if (!guild.PreferredLocale.IsNullOrWhiteSpace())
+            Locale = guild.PreferredLocale;
+
+        if (Locale is null && !t.ContainsKey("en"))
+            return "Missing Translation. Please report to the developer.";
+
+        Locale ??= "en";
+
+        return t[Locale];
+    }
+
     public string Get(DiscordUser user)
     {
-        if (t.ContainsKey(user?.Locale ?? "en"))
-            return t[user?.Locale ?? "en"];
-        else
-            return t["en"];
+        string? Locale = user.Locale;
+
+        if (Locale is null && !t.ContainsKey("en"))
+            return "Missing Translation. Please report to the developer.";
+
+        Locale ??= "en";
+
+        return t[Locale];
     }
 }
