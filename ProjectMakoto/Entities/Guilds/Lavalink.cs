c0050b9f070fb77abaa81928a7118ba86ba8ef8a
Mira <56395159+TheXorog@users.noreply.github.com>
2023-12-25 00:06:01 +0100

    fix: Check if list is empty before using .Contains

--- ProjectMakoto/Entities/Guilds/Lavalink.cs
+++ ProjectMakoto/Entities/Guilds/Lavalink.cs
@@ -228,7 +228,10 @@ public sealed class Lavalink(Bot bot, Guild parent) : RequiresParent<Guild>(bot,
 
                     var skipSongs = 0;
 
-                    if (LastPlayedTrack is not null && _bot.Guilds[this.Guild.Id].MusicModule.Repeat && _bot.Guilds[this.Guild.Id].MusicModule.SongQueue.Contains(LastPlayedTrack))
+                    if (LastPlayedTrack is not null && 
+                        _bot.Guilds[this.Guild.Id].MusicModule.Repeat && 
+                        _bot.Guilds[this.Guild.Id].MusicModule.SongQueue.IsNotNullAndNotEmpty() && 
+                        _bot.Guilds[this.Guild.Id].MusicModule.SongQueue.Contains(LastPlayedTrack))
                     {
                         skipSongs = Array.IndexOf(_bot.Guilds[this.Guild.Id].MusicModule.SongQueue, LastPlayedTrack) + 1;
 
