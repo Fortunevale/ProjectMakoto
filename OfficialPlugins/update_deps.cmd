7fd022f166941254eff46b4c25df1df511955182
Mira <56395159+TheXorog@users.noreply.github.com>
2024-01-20 14:25:44 +0100

    fix: idfk this was a stash

--- OfficialPlugins/update_deps.cmd
+++ OfficialPlugins/update_deps.cmd
@@ -17,6 +17,17 @@ rmdir /S /Q deps
 dotnet publish ..\ProjectMakoto\ProjectMakoto.sln --configuration RELEASE --runtime linux-x64 --no-self-contained --property:PublishDir="deps" --framework net8.0
 move ..\ProjectMakoto\deps deps
 
+set "original_dir=%CD%"
+cd /d "..\Dependencies"
+
+for /d /r %%i in (*deps*) do (
+    rd /s /q "%%i"
+)
+
+cd /d "%original_dir%"
+
+git submodule update --init --depth 0
+
 for /D %%i in (*) do (
 	if /I "%%i" neq "deps" (
 		if exist "%%i\.build.cmd" (
@@ -25,10 +36,10 @@ for /D %%i in (*) do (
 				mklink /d "%%i\deps" "..\deps"
 			)
 			
-			pushd "%%i"
+			cd "%%i"
+			
 			echo Syncing git submodules in %%i
-			git submodule update --init --recursive
-			popd
+			git submodule update --init --depth 0
 		)
 	)
 )
