7cb14c75645f47fe7e45ded1f78c061f766062d7
Mira <56395159+TheXorog@users.noreply.github.com>
2024-01-20 14:26:31 +0100

    fix: what the fuck am i doing

--- OfficialPlugins/update_deps.cmd
+++ OfficialPlugins/update_deps.cmd
@@ -1,18 +1,5 @@
 @echo off
 
-pushd ..
-git submodule update --init --recursive
-popd
-
-set "original_dir=%CD%"
-cd /d "..\Dependencies"
-
-for /d /r %%i in (*deps*) do (
-    rd /s /q "%%i"
-)
-
-cd /d "%original_dir%"
-
 rmdir /S /Q deps
 dotnet publish ..\ProjectMakoto\ProjectMakoto.sln --configuration RELEASE --runtime linux-x64 --no-self-contained --property:PublishDir="deps" --framework net8.0
 move ..\ProjectMakoto\deps deps
